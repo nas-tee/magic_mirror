241ae867d50c393239a399bc98a1dcef79618b91
Nathan Emslander <nemslander@Nathans-MacBook-Pro.local>
2016-08-18 12:52:38 -0500

    print the environmental variables for api_key, lat and lng

--- magic_mirror.py
+++ magic_mirror.py
@@ -3,3 +3,7 @@ import os
 api_key= os.environ['MAGIC_MIRROR_APIKEY']
 latitude= os.environ['MAGIC_MIRROR_LATITUDE']
 longitude= os.environ['MAGIC_MIRROR_LONGITUDE']
+
+print(api_key)
+print(latitude)
+print(longitude)
